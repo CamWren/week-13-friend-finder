ca646d438ad051331551516f10b9a9336f47c0f8
CamWren <wrnecmrn@gmail.com>
2017-02-06 21:10:15 -0600

    Insert arbitrary comment

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,81 @@
+var friends = [{
+  name:"Django Reinhardt",
+  photo:"https://en.wikipedia.org/wiki/Django_Reinhardt#/media/File:Django_Reinhardt_(Gottlieb_07301).jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  name:"Willie Nelson",
+  photo:"http://cdn.newsbake.com/wp-content/uploads/2016/05/maxresdefault-2.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  name:"Howlin' Wolf",
+  photo:"http://www.furious.com/perfect/wolf/graphics/hwolf-1.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  name:"Albert Lee",
+  photo:"https://www.calgaryfolkfest.com/assets/Uploads/Albert-Lee.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  name:"Eric Johnson",
+  photo:"http://mimmusictheater.themim.org/wp-content/uploads/2015/06/2015-08-10-Eric-Johnson3.jpg",
+  scores:[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}]
+
+
+module.exports = friends;
 
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,12 @@
-app.get("../public/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
+var path = require('path');
 
-app.use("../public/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
+module.exports = function(app) {
+	app.get('/survey', function(req, res) {
+  	  res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	app.use( function(req, res) {
+	  res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
+
